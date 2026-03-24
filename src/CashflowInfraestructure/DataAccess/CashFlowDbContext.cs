dcdc62bd997bfd3b5d21027d8bbd8206ea6cb5c0
JuanFila <juanfilapablo@hotmail.com>
2026-03-23 21:50:17 -0300

    alterações v2

---
+++ src/CashflowInfraestructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashflowInfraestructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost;database=cashflowdb;user=root;password=root";
+        var version = new Version(8, 0, 26);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(
+            connectionString,
+            serverVersion
+        );
+    }
+}
